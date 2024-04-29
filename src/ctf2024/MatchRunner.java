7d847f4c72538cfef3e7f9cbb169acaf8189c682
rober <robert.hettmansperger@bvsd.org>
2024-04-29 17:18:17 -0600

    Update for 2024

--- src/ctf2024/MatchRunner.java
+++ src/ctf2024/MatchRunner.java
@@ -11,13 +11,19 @@ public class MatchRunner {
 
     public static void main(String[] args) {
 
+        // Both Teams are built for the "left side"
+        // The Team assigned to the "right side" will automatically be mirrored when placed
+
         Team a = new SampleTeam("Red Team", Color.RED);
         Team b = new SampleTeam("Blue Team", Color.BLUE);
 
         a.setOpposingTeam(b);
         b.setOpposingTeam(a);
 
+        // Build a Match with limited number of rock clumps - in the "real" competition there will be 75
         Match match = new Match(a, b, 10);
+
+        // start the match
         match.start();
     }
 }
